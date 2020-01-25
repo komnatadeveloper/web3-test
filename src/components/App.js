65f1cc9fedca772bfc7c3e6b286d7fd6e142c107
komnatadeveloper <ozyazgane@hotmail.com>
2020-01-26 00:00:51 +0300

    26.01.2020 Finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,99 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
+import Web3 from 'web3';
+import Marketplace from '../abis/Marketplace.json';
 
 class App extends Component {
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+    console.log(window.web3);
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        'Non-Ethereum browser detected. You should consider trying Metamask!'
+      );
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    // Load account
+    const accounts = await web3.eth.getAccounts();
+    // console.log(accounts);
+    this.setState({ account: accounts[0] });
+    // console.log(Marketplace.abi, Marketplace.networks[5777].address);
+    const networkId = await web3.eth.net.gedId();
+    console.log(web3.eth.net);
+    const abi = Marketplace.abi;
+    const address = Marketplace.networks[networkId].address;
+    const marketplace = web3.eth.Contract(abi, address);
+    console.log(marketplace);
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: '',
+      productCount: 0,
+      products: [],
+      loading: true
+    };
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <nav className='navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow'>
           <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='navbar-brand col-sm-3 col-md-2 mr-0'
+            href='http://www.dappuniversity.com/bootcamp'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            Dapp University
+            Dapp University's BlockChain Marketplace
           </a>
+          <ul className='navbar-nav px-3'>
+            <li className='nav-item text-nowrap d-none d-sm-none d-sm-block'>
+              <small className='text-white'>
+                <span id='account'>{this.state.account}</span>
+              </small>
+            </li>
+          </ul>
         </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
+        <div className='container-fluid mt-5'>
+          <div className='row'>
+            <main role='main' className='col-lg-12 d-flex text-center'>
+              <div className='content mr-auto ml-auto'>
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  href='http://www.dappuniversity.com/bootcamp'
+                  target='_blank'
+                  rel='noopener noreferrer'
                 >
-                  <img src={logo} className="App-logo" alt="logo" />
+                  <img src={logo} className='App-logo' alt='logo' />
                 </a>
                 <h1>Dapp University Starter Kit</h1>
                 <p>
                   Edit <code>src/components/App.js</code> and save to reload.
                 </p>
                 <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  className='App-link'
+                  href='http://www.dappuniversity.com/bootcamp'
+                  target='_blank'
+                  rel='noopener noreferrer'
                 >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
+                  LEARN BLOCKCHAIN{' '}
+                  <u>
+                    <b>NOW! </b>
+                  </u>
                 </a>
               </div>
             </main>
